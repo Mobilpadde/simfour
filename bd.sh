8bc247f55c71d0279b1314e0d758fdbbe4b7b6af
Mads Cordes <Mobilpadde@gmail.com>
2018-04-28 15:40:01 +0200

    Move to static folder

--- bd.sh
+++ bd.sh
@@ -3,5 +3,5 @@ yarn install
 export NODE_ENV=production
 yarn build
 
-rm /home/mcordes/maweco/simFour -rf
-cp dist /home/mcordes/maweco/simFour/ -r
+rm /home/mcordes/maweco/static/simFour -rf
+cp dist /home/mcordes/maweco/static/simFour/ -r
